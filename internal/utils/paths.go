57372ed3d0e7b0d2e06f9e071c5913844441c2e3
sarth-shah20 <sarthshah333@gmail.com>
2026-03-27 10:27:27 +0530

    feat: update path

--- internal/utils/paths.go
+++ internal/utils/paths.go
@@ -14,11 +14,3 @@ func GetProjectVolumeDir(projectName string) (string, error) {
 	return filepath.Join(home, ".stasis", "volumes", projectName), nil
 }
 
-// returns: ~/.stasis/snapshots/<projectName>/<snapshotName>
-func GetSnapshotDir(projectName, snapshotName string) (string, error) {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-	return filepath.Join(home, ".stasis", "snapshots", projectName, snapshotName), nil
-}
